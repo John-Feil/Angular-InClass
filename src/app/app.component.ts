2b178b664fbbed22e4e398df787804d6055b9b7f
John-Feil <johnathancfeil@gmail.com>
2022-05-31 15:12:28 -0400

    trying to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title: string = 'Year-Up Students';
+  priceOfCart: number = 0;
+  itemsInCart: number = 0;
+  _nameFilter: string = "";
+
+  filteredStudent: any[] = [];
 
   student = [
 
@@ -15,7 +20,7 @@ export class AppComponent {
       "name": "John",
       "description": "Year-Up Student",
       "isHealthy": true,
-      "inStock": 3,
+      "inStock": 7,
       "price": 5.99,
       "image":"assets/profile-img.jpg",
       "featured": false,
@@ -27,8 +32,8 @@ export class AppComponent {
       "name": "Kyle",
       "description": "Year-Up Student",
       "isHealthy": true,
-      "inStock": 0,
-      "price": 7.99,
+      "inStock": 3,
+      "price": 11.99,
       "image": "assets/profile-img2.jpg",
       "featured": true,
       "qty": 0
@@ -40,35 +45,84 @@ export class AppComponent {
       "description": "Year-Up Student",
       "isHealthy": true,
       "inStock": 1,
-      "price": 8.99,
+      "price": 18.99,
       "image": "assets/profile-img3.jpg",
       "featured": false,
       "qty": 0
     }
   ]
 
-  totalInventory(){
+  set nameFilter(value: string) {
+    this._nameFilter = value;
+    console.log('In setter', value); 
+    this.filteredStudent = this.performFilter(value);
+}
 
-    let sum = 0;
-
-    for(let i of this.student){
-        sum += i.inStock;
-    }
-
-    return sum;
-
+  performFilter(filterBy: string): any[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.student.filter((student: any) =>
+    student.name.toLocaleLowerCase().includes(filterBy));
   }
 
-  increaseQTY(student: any){
+
+  increaseQTY(student: any) : void{
     if (student.qty < student.inStock){
       student.qty++;
     }
+    this.totalItemsAndPriceInCart();
   }
 
-  decreaseQTY(student: any){
+  decreaseQTY(student: any) : void{
     if (student.qty > 0){
       student.qty--;
     }
+    this.totalItemsAndPriceInCart();
+  }
+
+  totalItemsAndPriceInCart() : void{
+    let sum = 0;
+    let price = 0;
+    let tempSum = 0;
+    for(let i of this.student) {
+      tempSum = 0;
+      if (i.qty > 0){
+        sum += i.qty;
+        tempSum = i.qty;
+        price += (tempSum * i.price);
+      }
+    }
+    this.priceOfCart = price;
+    this.itemsInCart = sum;
+  }
+
+  qtyMinusStock(student: any) : number {
+    let sum = student.inStock - student.qty;
+    return sum;
+  }
+
+
+  checkValid(student: any) {
+    if (student.qty > student.inStock){
+      this.error('higher',student);
+    }
+    if (student.qty < 0) {
+      this.error('lower',student);
+    }
+  }
+
+  error(errmessage: string, student: any) : void {
+    let errormessage = `Error, you're inputed value is ${errmessage} than our current stock`
+    if (errmessage == 'higher') {
+      student.qty = student.inStock;
+    }
+    if (errmessage == 'lower'){
+      student.qty = 0;
+    }
+    alert(errormessage);
+  }
+
+  isEmptyObject(obj: any) {
+    return (obj && (Object.keys(obj).length === 0));
   }
 
 }
